59d1fe2549482faa764974ac1249e4acfb992a29
Nelson Atmani <natmani@pcalb-mm0000.univ-jfc.fr>
2021-03-31 11:14:41 +0200

    ajout recette

--- EasyCuisine/Visualiseur/Afficheur.java
+++ EasyCuisine/Visualiseur/Afficheur.java
@@ -1,6 +1,8 @@
 package Visualiseur;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.*;
@@ -28,7 +30,6 @@ public class Afficheur extends Application{
         
         FlowPane root = new FlowPane();  
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("style.css"); 
         root.setVgap(10);
         root.setHgap(10);  
         root.setPrefWrapLength(250);
@@ -41,35 +42,70 @@ public class Afficheur extends Application{
         Button accueil = new Button("",logo);
         Button recherche = new Button("Recherche");
         Button filtres = new Button("Filtres");
+        Button ajouter = new Button("Ajouter une recette");
+        
         filtres.setOnAction(new EventHandler<ActionEvent>() {  
-            
             @Override  
             public void handle(ActionEvent arg0) {
             	Stage filtre = new Stage();
-            	HBox box = new HBox(); 
+            	FlowPane box = new FlowPane(); 
             	Scene scene2 = new Scene(box);
             	filtre.setWidth(600);
             	filtre.setHeight(360);
             	filtre.setTitle("Filtres");
-            	Label categories = new Label("Catégories :");  
+            	Label categories = new Label("Catégories :");
                 CheckBox c1 = new CheckBox("Entrées");  
                 CheckBox c2 = new CheckBox("Plats");  
                 CheckBox c3 = new CheckBox("Desserts");  
                 CheckBox c4 = new CheckBox("Appéritifs");  
                  
                 box.getChildren().addAll(categories,c1,c2,c3,c4);  
-                box.setSpacing(5);
                 
                 filtre.setScene(scene2);
             	filtre.show();
-                System.out.println("Button clicked");  
-                  
             }  
-        } );  
-        root.getChildren().add(accueil);  
-        root.getChildren().add(texte); 
-        root.getChildren().add(recherche); 
-        root.getChildren().add(filtres);  
+        });
+        
+        ajouter.setOnAction(new EventHandler<ActionEvent>()  {  
+            @Override  
+            public void handle(ActionEvent arg0) {
+            	Stage ajout = new Stage();
+            	FlowPane box2 = new FlowPane();
+            	Scene scene2 = new Scene(box2);
+            	ajout.setWidth(600);
+            	ajout.setHeight(360);
+            	box2.setVgap(6);
+            	box2.setHgap(10);
+            	ajout.setTitle("Ajouter une recette");
+            	Label t = new Label("Nom de la recette : ");
+            	TextField titre = new TextField();
+            	Button img = new Button("Image");
+            	Label r = new Label("Etape 1 : ");
+            	TextField prep = new TextField();
+            	Region p = new Region();
+            	p.setPrefSize(Double.MAX_VALUE, 0.0);
+            	FileInputStream input2=null;
+				try {
+					input2 = new FileInputStream("ressources/images/star.png");
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+                Image star = new Image(input2);
+                ImageView logo2 = new ImageView(star);
+                logo2.setFitHeight(30);
+                logo2.setFitWidth(30);
+                 
+                box2.getChildren().addAll(t,titre,img);
+                box2.getChildren().add(p);
+                box2.getChildren().addAll(r,prep,logo2);
+                
+                ajout.setScene(scene2);
+                ajout.show();   
+            }  
+        });
+        
+        
+        root.getChildren().addAll(accueil,texte,recherche,filtres,ajouter); 
         
         stage.setScene(scene);
 
